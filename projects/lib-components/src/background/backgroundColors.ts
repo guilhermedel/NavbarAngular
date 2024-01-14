0a6613cee19537ef9c4ca360855a913d38085fae
guilhermedel <glhrmgithub@gmail.com>
2024-01-14 11:12:24 -0300

    feat: creating menu list

---
+++ projects/lib-components/src/background/backgroundColors.ts
@@ -0,0 +1,5 @@
+export const backgroundColors:{ [key: string]: string } = {
+    dark:'#212121',
+    light:'#c9c9c9',
+    custom:''
+  };
--- projects/lib-components/src/lib/navbar/navbar.component.ts
+++ projects/lib-components/src/lib/navbar/navbar.component.ts
@@ -1,16 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { fontSizes } from '../../fonts/fontSizes';
+import { backgroundColors } from '../../background/backgroundColors';
 
 @Component({
   selector: 'lib-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnChanges {
   faBars = faBars;
+  fontSizes = fontSizes;
+  backgroundColors = backgroundColors;
   showMenu: boolean = false;
-  @Input() backgroundColor: string | 'dark' | 'light';
+  @Input() backgroundColor: string | 'dark' | 'light' = 'dark';
   @Input() textColor: string;
   @Input() imageColor: string;
   @Input() sidenav: boolean = false;
@@ -19,7 +22,18 @@ export class NavbarComponent {
   @Input() image: 'sm' | 'md' | 'lg' = 'md';
   @Input() fontSize: 'sm' | 'md' | 'lg' = 'md';
 
+  constructor(private cdRef: ChangeDetectorRef) { }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['backgroundColor']) {
+      this.backgroundColors['custom'] = this.backgroundColor;
+      // this.cdRef.detectChanges();
+    }
+    console.log(this.backgroundColors)
+  }
+
   handleShowMenu() {
     this.showMenu = !this.showMenu;
+    console.log(this.showMenu);
   }
 }
